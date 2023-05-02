f1b5df7b5edcf59b76c07fe938d23f554fd74ada
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-05-02 15:31:43 +0300

    Create the user auth

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::API
+
+    respond_to :json
+    include ActionController::MimeResponds
+    
+
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:img_url, :is_admin, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:img_url, :is_admin, :email, :password, :current_password)}
+  end
+end
---
+++ db/migrate/20230501155457_add_name_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :img_url, :string
+    add_column :users, :is_admin, :boolean
+  end
+end
